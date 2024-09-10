75a201d90fc68939f22b7b3f9e6a2fd8f0bb1caf
ryan2817391827893 <joealt9the1q@gmail.com>
2024-09-10 14:27:46 -0700

    Update ClockDisplaySeconds.java

--- src/main/java/ClockDisplaySeconds.java
+++ src/main/java/ClockDisplaySeconds.java
@@ -1,6 +1,42 @@
+package src.main.java;
+
 
 public class ClockDisplaySeconds
 {
-    //Extend ClockDisplaySeconds to include a seconds field.
-    //Update all methods and parameters to accomodate this change
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private NumberDisplay seconds;
+    private String display;
+    public ClockDisplaySeconds(){
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        seconds = new NumberDisplay(60);
+        updateDisplay();
+    }
+    public void timeTick(){
+        seconds.increment();
+        if(seconds.getValue()==0)
+        {
+            minutes.increment();
+        }
+        if(minutes.getValue()==0){
+            hours.increment();
+        }
+        updateDisplay();
+    }
+    public String getTime(){
+        return display;
+    }
+    public void updateDisplay(){
+        String output = "";
+        
+        output = hours.getDisplayValue()+":"+minutes.getDisplayValue()+":"+seconds.getDisplayValue();
+        display = output;
+    }
+    public void setTime(int h, int m, int s){
+        hours.setValue(h);
+        minutes.setValue(m);
+        seconds.setValue(s);
+        updateDisplay();
+    }
 }
